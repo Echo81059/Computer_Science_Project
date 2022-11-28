ae2849a18b7ca06ed16fc40959ab46abbdd0e73a
Morgan Fisher <morgantf2000@gmail.com>
2022-11-28 13:17:43 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-# importing the modules
+# importing the packages
 import numpy as np
 import pygame
 import random
 import time
 
-# Setting dimensions of both the boards and pixels
-# Setting time variable
-# Creating player and tail lists
+#Setting dimensions
 PIXEL_LENGTH = 850
 BOARD_LENGTH = 64
 scale_ratio = PIXEL_LENGTH/BOARD_LENGTH
@@ -15,25 +13,12 @@ players = []
 tail = []
 
 # define food object
-# __init__ function operates normally, assgins values to postion and energy
 class Food():
     def __init__(self, position: list, energy=1):
         self.position = position
         self.energy = energy
 
-# Creating the Snake Class
-# __init__ function operates normally, assigning values to object properties
-# change_direction function outputs the value assigned to the parameter dir
-# grow function sets grow object property to true
-# die function sets 2 global variables.
-# Creates an if function that if the global running variable is false the snake "dies" and is removed
-# move function is where we create the base for the snakes movements
-# We assign values to the snakes direction and if facing that direction, the snake will "move" in this direction
-# We accomplish this by adding and removing values from the head and tail, which will eventually look like adding and removing shapes
-# If the head reaches the end of the board area - the snkae die function is triggered
-# If the head and another part of the tail occupy the same area - the snake die function is triggered
-# If the head and food occupies the same area - the snake grow function is triggered, the food is removed, and then food position changes
-
+# define snake object
 class Snake():
     def __init__(self, direction=0, head=[0, 0], tail=[[0, 0]], snakeID=0):
         self.snakeID = snakeID
@@ -50,7 +35,6 @@ class Snake():
 
     def grow(self):
         self.grow = True
-        pygame.display.set_caption(len(self.tail)
 
     def die(self):
         global running
@@ -66,6 +50,7 @@ class Snake():
             return
         elif self.grow:
             self.grow = False
+            pygame.display.set_caption('score: ' + str(len(self.tail)))
         else:
             self.tail.pop(len(self.tail)-1)
         if self.direction == -2: # left
@@ -85,11 +70,9 @@ class Snake():
             for i in self.tail:
                 i[0] -= 1
         self.tail.insert(0, [0, 0])
-        #Checks if the snake goes out of bounds 
         if abs(self.head[0]-(BOARD_LENGTH/2)) > BOARD_LENGTH/2 or \
         abs(self.head[1]-(BOARD_LENGTH/2)) > BOARD_LENGTH/2 or [0, 0] in self.tail[1:]:
             self.die()
-        # Checks if the snake is close to food
         for i in foods:
             if abs(self.head[0] - i.position[0]) < 2 and abs(self.head[1] - i.position[1]) < 2:
                 self.grow = True
@@ -102,40 +85,29 @@ players.append(user)
 foods = []
 foods.append(Food(position=[random.randint(1, BOARD_LENGTH-1), random.randint(1, BOARD_LENGTH-1)]))
 
-# update_gui function
 def update_gui(players):
-    # Creates 2 global variables moved and tail
     global moved
     global tail
-    # Fills the screen
     scr.fill((255, 255, 255))
-    # Creates the player head
     for player in players:
         pygame.draw.circle(scr, (255, 0, 0), [i*scale_ratio for i in player.head], scale_ratio)
-        # Adds more cells values to the snake
         if moved:
             tail = []
             for i in player.tail:
                 snake_cell = []
-                # Scaling the snake to the window size
                 snake_cell.append((i[0]+player.head[0])*scale_ratio)
                 snake_cell.append((i[1]+player.head[1])*scale_ratio)
                 tail.insert(0, snake_cell)
-        # Drawing the circles
         for i in tail:
             pygame.draw.circle(scr, (30, 255, 50), i, scale_ratio*1.5)
-    # Based on the length/number of the foods "eaten" 
     if len(foods) >= 1:
         for food in foods:
             pygame.draw.circle(scr, (255, 0, 0), [i*scale_ratio for i in food.position], scale_ratio*.75)
     moved = False
 
-# Instantiates the board
 pygame.init()
 scr = pygame.display.set_mode((PIXEL_LENGTH, PIXEL_LENGTH))
 running = True
-# Creating the controls based on the arrow keys - setting the directions with values
-# The while running basically means for these commands to go on the entire time when
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -149,9 +121,7 @@ while running:
                  user.change_direction(1)
             if event.key == pygame.K_RIGHT and user.direction != -2:
                  user.change_direction(2)
-    # Once the snake spawns in, it is stationary
-    # Allows the player to move once the game begins
-    if time.time()-t > 0.03:
+    if time.time()-t > 0.00:
         t = time.time()
         user.move()
         moved = True
